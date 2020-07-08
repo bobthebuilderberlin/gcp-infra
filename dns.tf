6ae7e1b3c202db20dc4ccca0858fc2293cd30adf
Robert Andruschow <robert.andruschow@googlemail.com>
2020-07-08 17:14:42 +0200

    Switch to andruschow.de

--- dns.tf
+++ dns.tf
@@ -2,3 +2,8 @@ resource "google_dns_managed_zone" "prod" {
   name     = "prod-zone"
   dns_name = "robertandruschow.de."
 }
+
+resource "google_dns_managed_zone" "prod2" {
+  name     = "prod-zone2"
+  dns_name = "andruschow.de."
+}
